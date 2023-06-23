ae56d56e7bd46408f6e020755e97465c61b05655
MD-XI <box.by.x1@gmail.com>
2023-06-23 17:11:09 +0200

    fist

---
+++ main.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using CsvHelper;
+using CsvHelper.Configuration;
+using Newtonsoft.Json;
+
+namespace CsvChart
+{
+    class Program
+    {
+        static void Main()
+        {
+}
+    }
+}
